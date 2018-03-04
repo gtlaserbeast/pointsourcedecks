9138825b129fbe31cdbf42c7a5052f2d4ec5f732
Will Fisher <willcfisherjr@gmail.com>
2018-03-04 16:00:53 -0500

    posting with issues

--- app/routes/random_deck_routes.js
+++ app/routes/random_deck_routes.js
@@ -11,26 +11,43 @@ module.exports = function(app, db) {
   //     }
   //   });
   // });
-
-app.post('/randomDeck', (req, res) => {
+  const collection = app.post('/randomDeck', (req, res) => {
     const getRandomURL = 'http://applicant.pointsource.us/api/random/5a95a6a5d625c81979aa798c',
           min = 1,
           max = 53;
     var generatedArray = {};
     request(getRandomURL + '?min=' + min + '&max=' + max + '&num=' + (max - 1), function (error, response, body) {
-      console.log('error:', error);
-      console.log('statusCode:', response && response.statusCode);
-      console.log('body:', body);
-      generatedArray = body;
+      generatedArray = JSON.parse(body);
+      console.log(generatedArray.numbers)
     });
-    const deck = generatedArray.numbers;
-    res.send();
-    // db.collection('decks').insert(deck, (err, result) => {
-    //   if (err) { 
-    //     res.send({ 'error': 'An error has occurred' }); 
-    //   } else {
-    //     res.send(result.ops[0]);
-    //   }
-    // });
+    db.collection('decks').insert({'cards' : generatedArray.numbers}, (err, result) => {
+      if (err) { 
+        res.send({ 'error': 'An error has occurred' }); 
+      } else {
+        res.send(result.ops[0]);
+      }
+    });
   });
+
+// app.post('/randomDeck', (req, res) => {
+//     const getRandomURL = 'http://applicant.pointsource.us/api/random/5a95a6a5d625c81979aa798c',
+//           min = 1,
+//           max = 53;
+//     var generatedArray = {};
+//     request(getRandomURL + '?min=' + min + '&max=' + max + '&num=' + (max - 1), function (error, response, body) {
+//       console.log('error:', error);
+//       console.log('statusCode:', response && response.statusCode);
+//       console.log('body:', body);
+//       generatedArray = body;
+//     });
+//     const deck = generatedArray.numbers;
+//     res.send();
+//     // db.collection('decks').insert(deck, (err, result) => {
+//     //   if (err) { 
+//     //     res.send({ 'error': 'An error has occurred' }); 
+//     //   } else {
+//     //     res.send(result.ops[0]);
+//     //   }
+//     // });
+//   });
 };
