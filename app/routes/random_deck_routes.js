0a299c472b5b19dd05e7825f1fd68897d873391d
Will Fisher <willcfisherjr@gmail.com>
2018-03-04 17:44:07 -0500

    Removed db stuff because of change to local storage gitignore update

--- app/routes/random_deck_routes.js
+++ app/routes/random_deck_routes.js
@@ -13,7 +13,8 @@ module.exports = function(app) {
   });
 
   app.post('/randomDeck', (req, res) => {
-    var storage;
+    var storage = {};
+    //check to see if there are already decks
     fs.readFile('./decks.json', 'utf8', function readFileCallback(err, data){
       if (err){
         console.log(err);
@@ -22,14 +23,16 @@ module.exports = function(app) {
       }
     });
 
+    //call pointsource for randomness
     const getRandomURL = 'http://applicant.pointsource.us/api/random/5a95a6a5d625c81979aa798c',
           min = 1,
           max = 53;
     var generatedArray = {};
     request(getRandomURL + '?min=' + min + '&max=' + max + '&num=' + (max - 1), function (error, response, body) {
-      generatedArray = JSON.parse(body);
-      storage.decks.push(generatedArray.numbers);
-      fs.writeFile('./decks.json', JSON.stringify(storage), 'utf8');
+      generatedArray = JSON.parse(body); //JSONify deck being created
+      storage.decks.push(generatedArray.numbers); //store created deck's numbers
+      fs.writeFile('./decks.json', JSON.stringify(storage), 'utf8'); //write full list of decks
+      console.log('Created deck #' + storage.decks.length);
     });
   });
 };
--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express        = require('express');
-const MongoClient    = require('mongodb').MongoClient;
 const bodyParser     = require('body-parser');
 const request        = require('request');
 const cors           = require('cors');
@@ -12,14 +11,8 @@ const port = 8000;
 app.use(cors());
 app.use(bodyParser.json());
 
-MongoClient.connect(db.url, (err, database) => {
-  if (err) return console.log(err)
-                      
-  // Make sure you add the database name and not the collection name
-  db = database.db("random-deck")
-  require('./app/routes')(app, db);
+require('./app/routes')(app, db);
 
-  app.listen(port, () => {
-    console.log('It is happening on port ' + port);
-  });               
-})
+app.listen(port, () => {
+  console.log('It is happening on port ' + port);
+});
