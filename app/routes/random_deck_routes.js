f5585c10108fad8e3696b0f0c6c11de18971c443
Will Fisher <willcfisherjr@gmail.com>
2018-03-04 17:10:14 -0500

    saving to fs

--- app/routes/random_deck_routes.js
+++ app/routes/random_deck_routes.js
@@ -1,53 +1,26 @@
 
 module.exports = function(app, db) {
-  var request = require('request');
-  // app.get('/deck/:id', (req, res) => {
-  //   const details = { '_id': req };
-  //   db.collection('decks').findOne(details, (err, item) => {
-  //     if (err) {
-  //       res.send({'error':'An error has occurred'});
-  //     } else {
-  //       res.send(item);
-  //     }
-  //   });
-  // });
-  const collection = app.post('/randomDeck', (req, res) => {
+  var request = require('request'),
+      fs = require('fs');
+
+  app.post('/randomDeck', (req, res) => {
+    var storage;
+    fs.readFile('./decks.json', 'utf8', function readFileCallback(err, data){
+      if (err){
+        console.log(err);
+      } else {
+        storage = JSON.parse(data);
+      }
+    });
+
     const getRandomURL = 'http://applicant.pointsource.us/api/random/5a95a6a5d625c81979aa798c',
           min = 1,
           max = 53;
     var generatedArray = {};
     request(getRandomURL + '?min=' + min + '&max=' + max + '&num=' + (max - 1), function (error, response, body) {
       generatedArray = JSON.parse(body);
-      console.log(generatedArray.numbers)
-    });
-    db.collection('decks').insert({'cards' : generatedArray.numbers}, (err, result) => {
-      if (err) { 
-        res.send({ 'error': 'An error has occurred' }); 
-      } else {
-        res.send(result.ops[0]);
-      }
+      storage.decks.push(generatedArray.numbers);
+      fs.writeFile('./decks.json', JSON.stringify(storage), 'utf8');
     });
   });
-
-// app.post('/randomDeck', (req, res) => {
-//     const getRandomURL = 'http://applicant.pointsource.us/api/random/5a95a6a5d625c81979aa798c',
-//           min = 1,
-//           max = 53;
-//     var generatedArray = {};
-//     request(getRandomURL + '?min=' + min + '&max=' + max + '&num=' + (max - 1), function (error, response, body) {
-//       console.log('error:', error);
-//       console.log('statusCode:', response && response.statusCode);
-//       console.log('body:', body);
-//       generatedArray = body;
-//     });
-//     const deck = generatedArray.numbers;
-//     res.send();
-//     // db.collection('decks').insert(deck, (err, result) => {
-//     //   if (err) { 
-//     //     res.send({ 'error': 'An error has occurred' }); 
-//     //   } else {
-//     //     res.send(result.ops[0]);
-//     //   }
-//     // });
-//   });
 };
