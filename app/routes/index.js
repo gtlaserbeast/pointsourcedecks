0f7c428fb3541827685c52320def0f5baf19032c
Will Fisher <willcfisherjr@gmail.com>
2018-03-04 18:25:44 -0500

    reset decks.json removed route i ended up not using

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,5 @@
 const randomDeckRoutes = require('./random_deck_routes');
-const randomNumberRoutes = require('./random_number_routes');
 
 module.exports = function(app) {
   randomDeckRoutes(app);
-  randomNumberRoutes(app);
 };
